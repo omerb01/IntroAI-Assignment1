3cf31397ec66ee29af223d415c43f6abcc3b2fe8
Oran Nahoum <saintoran@gmail.com>
2018-11-17 18:16:13 +0200

    Ex 8 and  9.

--- framework/graph_search/astar.py
+++ framework/graph_search/astar.py
@@ -68,13 +68,19 @@ class AStar(BestFirstSearch):
                   but still could be improved.
         """
 
-        if self.close.has_state(successor_node.state):
-            return
-
         if self.open.has_state(successor_node.state):
             already_found_node_with_same_state = self.open.get_node_by_state(successor_node.state)
             if already_found_node_with_same_state.expanding_priority > successor_node.expanding_priority:
                 self.open.extract_node(already_found_node_with_same_state)
+                self.open.push_node(successor_node)
 
-        if not self.open.has_state(successor_node.state):
+        elif self.close.has_state(successor_node.state):
+            already_found_node_with_same_state = self.close.get_node_by_state(successor_node.state)
+            if already_found_node_with_same_state.expanding_priority > successor_node.expanding_priority:
+                self.close.extract_node(already_found_node_with_same_state)
+                self.open.push_node(successor_node)
+
+        else:
             self.open.push_node(successor_node)
+
+
