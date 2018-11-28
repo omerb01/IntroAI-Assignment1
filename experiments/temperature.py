b8669d39b4f3215a369abd887650e28db8c1d8d0
Oran Nahoum <saintoran@gmail.com>
2018-11-28 15:48:22 +0200

    Finished 22 ~!~

--- experiments/temperature.py
+++ experiments/temperature.py
@@ -4,12 +4,24 @@ from matplotlib import pyplot as plt
 X = np.array([400, 450, 900, 390, 550])
 
 # TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+
+T = np.linspace(0.01,5,100)
+P = np.zeros((100,5))
+min_x=min(X)
+
+for i in range(len(X)) :
+    for t in range(len(T)):
+        sum = 0
+        for j in range(len(X)):
+            sum += np.power(X[j]/min_x,((-1)/T[t]))
+        probability = np.power(X[i]/min_x,((-1)/T[t]))/sum
+        P[t][i]=probability
 
 print(P)
 
 for i in range(len(X)):
     plt.plot(T, P[:, i], label=str(X[i]))
+
 
 plt.xlabel("T")
 plt.ylabel("P")
